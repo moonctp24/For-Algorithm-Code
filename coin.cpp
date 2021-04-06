fafe45e6d427455d37fa4f11bc1bed00672e1bb6
moonctp24 <67907517+moonctp24@users.noreply.github.com>
2021-04-06 14:45:10 +0900

    BAEKJOON 11047번 동전 0

---
+++ coin.cpp
@@ -0,0 +1,30 @@
+/*
+BAEKJOON 11047번 동전 0
+https://www.acmicpc.net/problem/11047
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(void){
+    int n, k;
+    cin >> n >> k;
+    vector <int> v;
+    for(int i=0;i<n;i++){
+        int n=0;
+        cin >> n;
+        v.push_back(n);
+    }
+
+    int count=0;
+    int num = v.size(); // 밑에 for문에 i<v.back() 바로 넣었더니 v의 마지막 원소를 꺼낼 때마다 사이즈가 줄어들어서 반복 횟수에 영향을 미침
+
+    for(int i=0;i<num;i++){
+        count += k/v.back();
+        k = k%v.back();
+        v.pop_back();
+    }
+    cout << count<<endl;
+}
