daf0e1d2aa7c46b67751363d4cde9a0ab345cb92
moonctp24 <67907517+moonctp24@users.noreply.github.com>
2021-05-16 11:35:37 +0900

    Programmers TARGET NUMBER
    반복문 사용
    O(2^n)
    bad alloc error

---
+++ C++/Programmers/target_number.cpp
@@ -0,0 +1,40 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+// O(2^n) 방법밖에 생각안나는데..
+// bad alloc Error
+
+using namespace std;
+
+int solution(vector<int> numbers, int target) {
+    vector<int> temp;
+    
+    temp.push_back(0);
+    
+    for(int i=0;i<numbers.size();i++){
+        for(int j=(temp.size())/2;j<temp.size();j++){
+            temp.push_back(temp[j] + numbers[i]);
+            temp.push_back(temp[j] - numbers[i]);
+        }
+    }
+
+    for(int i=0;i<temp.size();i++)
+        cout << temp[i]<<"  ";
+        
+    // int n =0;
+    // int temp_size = temp.size();
+    // for(int i=temp_size/2; i<temp_size;i++)
+    //     if(temp[i] == target) n++;
+    
+    
+    int answer = 0;
+    return answer;
+}
+
+int main(void){
+    vector<int> numbers1{1, 1, 1};
+    int target1 = 2;
+    int return1 = 3;
+    if(solution(numbers1, target1) == return1) cout << "right"<<endl;
+}
