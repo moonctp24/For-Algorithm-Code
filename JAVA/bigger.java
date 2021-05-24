b3d8c81e2cb15ad6290538181ff4d7bcd4dacee4
moonctp24 <67907517+moonctp24@users.noreply.github.com>
2021-05-24 16:28:54 +0900

    Study DFS (Programmers Network)

---
+++ JAVA/bigger.java
@@ -0,0 +1,26 @@
+// 스택 쓰더라
+
+import java.util.Scanner;
+
+public class bigger {
+
+    public int find_min_index(String str){
+        int min;
+        int index=0;
+        for(int i=0;i<str.length();i++) {                
+            // if(min>str[i]) {
+            // //min의 값보다 array[i]이 작으면 min = array[i]
+            // min = str[i];
+            // }
+        }
+        return index;
+    }
+    public static void main(String[] args){
+        int n, k;
+        String str;
+        Scanner scan = new Scanner(System.in);
+        n = scan.nextInt();
+        k = scan.nextInt();
+        str = scan.next();
+    }
+}
