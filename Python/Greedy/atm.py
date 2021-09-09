daaa86fe7c20e6a21636abd2f75a6fb9a8e9b083
moonctp24 <67907517+moonctp24@users.noreply.github.com>
2021-09-09 16:30:22 +0900

    Baekjoon 11399

--- Python/Greedy/atm.py
+++ Python/Greedy/atm.py
@@ -15,3 +15,19 @@ for i in range(len(human)):
 print(sum)
 
 # C++은 2015KB, 0ms 썼는데 Python은 29200KB, 128ms 썼다.. 확실히 C++이 빠르고 효율이 좋네
+
+"""
+# atm 2nd
+
+n = int(input())
+human = list(map(int, input().split()))
+
+human.sort()
+
+sum = 0
+for i in range(0, len(human)):
+    for j in range(0, i+1):
+        sum = sum + human[j]
+print(sum)
+
+"""
