bda0642ee7ee772e2d20b75009b345487d000204
moonctp24 <67907517+moonctp24@users.noreply.github.com>
2021-09-14 16:53:16 +0900

    PROGRAMMERS SQL JOIN

---
+++ SQL/spayed.sql
@@ -0,0 +1,9 @@
+/*
+https://programmers.co.kr/learn/courses/30/lessons/59045?language=mysql
+PROGRAMMERS SQL JOIN 보호소에서 중성화한 동물
+*/
+
+SELECT A.ANIMAL_ID, A.ANIMAL_TYPE, A.NAME
+FROM ANIMAL_INS A JOIN ANIMAL_OUTS B
+ON A.ANIMAL_ID = B.ANIMAL_ID
+WHERE A.SEX_UPON_INTAKE LIKE 'Intact%' AND (B.SEX_UPON_OUTCOME LIKE 'Spayed%' OR B.SEX_UPON_OUTCOME LIKE 'Neutered%')
