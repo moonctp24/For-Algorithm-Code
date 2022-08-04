9ca88d80855a5960ecb6bd2841aecf3041b768ff
Kwon YR <moonctp24@gmail.com>
2022-08-04 12:46:37 +0900

    Programmers 숫자 문자열과 영단어 문제 풀이

---
+++ Python/programmersLv.1/numStrWord.py
@@ -0,0 +1,34 @@
+"""
+https://school.programmers.co.kr/learn/courses/30/lessons/81301?language=python3
+PROGRAMMERS 숫자 문자열과 영단어
+"""
+
+def solution(s):
+    words=['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
+    answer = ''
+    for i, num in enumerate(words):
+        if num in words:
+            s = s.replace(num, str(i))
+        answer = s
+    
+    return int(answer)
+
+def main():
+    s1 = "one4seveneight"
+    s2 = "23four5six7"
+    s3 = "2three45sixseven"
+    s4 = "123"
+
+    result1 = 1478
+    result2 = 234567
+    result3 = 234567
+    result4 = 123
+
+    if solution(s1) == result1: print("RIGHT1")
+    if solution(s2) == result2: print("RIGHT2")
+    if solution(s3) == result3: print("RIGHT3")
+    if solution(s4) == result4: print("RIGHT4")
+
+if __name__=="__main__":
+    main()
