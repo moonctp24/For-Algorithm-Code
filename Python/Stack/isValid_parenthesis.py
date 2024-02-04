d83843a5c2dc4e1937333acbd82b2c9b80ed0e61
moonctp <moonctp24@gmail.com>
2024-02-04 10:35:16 +0900

    stack 괄호 문제 추가

---
+++ Python/Stack/isValid_parenthesis.py
@@ -0,0 +1,29 @@
+'''
+제약조건
+1. 1 <= s.length <= 10^4
+2. s는 괄호만 담겨있다.
+'''
+
+# using stack
+def isValidParenthesis(s):
+    pStack = []
+    
+    for p in s:
+        if(p == '(' or p == '[' or p == '{'):
+            pStack.append(p)
+        else:
+            tmpP = pStack.pop()
+            if(p == ')' and tmpP == '('):
+                continue
+            elif(p == '}' and tmpP == '{'):
+                continue
+            elif(p == ']' and tmpP == '['):
+                continue
+    if(len(pStack)==0):
+        return True
+    else:
+        return False
+
+print(isValidParenthesis('(({}[]))'))
+print(isValidParenthesis('(({}[{]))'))
+print(isValidParenthesis('(({}[]())'))
